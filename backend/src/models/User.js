ba034763fe7641edd3708e46bfd32be52ff09564
alara <alarahkk@gmail.com>
2025-11-27 22:58:10 -0500

    git commit -m "setup"

---
+++ backend/src/models/User.js
@@ -0,0 +1,6 @@
+//import mongoose, user documentation on mondgodb
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+//define user schema
+// blueprint for user data structure in the database
